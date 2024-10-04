c907ae7e17f2d59276c40b82b06ea06b1f7aa730
pukinidev <v.caro@uniandes.edu.co>
2024-10-04 06:41:05 -0500

    responsive <3

--- app/components/pagecomponents/Home/Hero.tsx
+++ app/components/pagecomponents/Home/Hero.tsx
@@ -1,8 +1,7 @@
-'use client'
+"use client";
 
-import { Box} from "@mui/material";
+import { Box } from "@mui/material";
 import Image from "next/image";
-
 
 export default function Hero() {
   return (
@@ -11,14 +10,16 @@ export default function Hero() {
         width: "100%",
         paddingTop: 2,
         display: "flex",
+        justifyContent: "center", // Optional: center the image
       }}
     >
       <Image
         src="/banner.png"
         alt="Logo"
-        height={620}
-        width={1920}
-        style={{ marginBottom: "3rem" }}
+        layout="responsive" 
+        height={620} 
+        width={1920} 
+        style={{ marginBottom: "3rem", maxWidth: "100%", height: "auto" }} 
       />
     </Box>
   );
