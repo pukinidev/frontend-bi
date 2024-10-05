c876f199434ab58f53394b4e25709602c821b731
pukinidev <v.caro@uniandes.edu.co>
2024-10-05 02:45:15 -0500

    fix

--- app/model/layout.tsx
+++ app/model/layout.tsx
@@ -1,4 +1,3 @@
-import { Box } from "@mui/system";
 import { Container } from "@mui/material";
 
 export default function ModelLayout({
@@ -7,24 +6,16 @@ export default function ModelLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        paddingTop: 10,
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
       <Container
         maxWidth="xl"
         sx={{
           display: "flex",
+          paddingTop: 10,
           flexDirection: "column",
           paddingBottom: "3rem",
         }}
       >
         <section>{children}</section>
       </Container>
-    </Box>
   );
 }
