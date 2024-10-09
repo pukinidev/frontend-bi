2be7a162293a26b48cdc8c754712d90720dce96e
pukinidev <v.caro@uniandes.edu.co>
2024-10-09 17:28:08 -0500

    custom types

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+export {};
+
+declare global {
+    interface PredictionData {
+        "3": number;
+        "4": number;
+        "5": number;
+        Texto: string;
+        Prediccion: string;
+    }
+}
+
