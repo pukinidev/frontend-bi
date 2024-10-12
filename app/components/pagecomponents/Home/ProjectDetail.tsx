d0781d86fc83cbd19635252ff4f5edfacc7dd337
pukinidev <v.caro@uniandes.edu.co>
2024-10-12 18:33:22 -0500

    FIX

--- app/components/pagecomponents/Home/ProjectDetail.tsx
+++ app/components/pagecomponents/Home/ProjectDetail.tsx
@@ -1,7 +1,17 @@
 "use client";
 
-import { Container, Grid2, Typography, ListItem, List } from "@mui/material";
+import {
+  Container,
+  Grid2,
+  Typography,
+  ListItem,
+  List,
+  Card,
+  Button,
+} from "@mui/material";
 import Image from "next/image";
+import Link from 'next/link';
+
 
 export default function ProjectDetail() {
   return (
@@ -34,7 +44,6 @@ export default function ProjectDetail() {
         2023)​. El propósito es permitir un análisis más eficiente y económico
         de las opiniones ciudadanas para encontrar insights que permitan la toma
         de decisiones y el diseño de políticas públicas.
-
       </Typography>
       <Typography
         variant="h3"
@@ -58,9 +67,14 @@ export default function ProjectDetail() {
         </ListItem>
       </List>
 
-      <Typography variant="h3" color="primary" fontWeight="bold" sx={{
-        paddingBottom: "1rem",
-      }}>
+      <Typography
+        variant="h3"
+        color="primary"
+        fontWeight="bold"
+        sx={{
+          paddingBottom: "1rem",
+        }}
+      >
         ODS
       </Typography>
       <Grid2
@@ -76,6 +90,114 @@ export default function ProjectDetail() {
         <Image src="/ODS3.png" alt="ODS 3" height={300} width={300} />
         <Image src="/ODS4.png" alt="ODS 4" height={300} width={300} />
         <Image src="/ODS5.png" alt="ODS 5" height={300} width={300} />
+      </Grid2>
+      <Typography
+        variant="h3"
+        color="primary"
+        fontWeight="bold"
+        sx={{ marginBottom: "2rem" }}
+      >
+        Servicios
+      </Typography>
+      <Grid2
+        container
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: "8rem",
+          gap: 3,
+        }}
+      >
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "1rem",
+            width: "300px",
+            minHeight: "200px",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h5" color="primary" fontWeight="bold">
+            Reentrenar el modelo
+          </Typography>
+          <Typography variant="body1" sx={{ wordWrap: "break-word" }}>
+            El reentrenamiento de un modelo de clasificación de ODS (Objetivos
+            de Desarrollo Sostenible) implica ajustar el modelo original
+            utilizando nuevos datos.
+          </Typography>
+          <Link href="/retrainmodel" passHref>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ marginTop: "1rem", color: "white" }}
+            >
+              Probar
+            </Button>
+          </Link>
+        </Card>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "1rem",
+            width: "300px",
+            minHeight: "200px",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h5" color="primary" fontWeight="bold">
+            Predecir un archivo
+          </Typography>
+          <Typography variant="body1" sx={{ wordWrap: "break-word" }}>
+            Predice las opiniones ciudadanas respecto a los ODS 3, 4 y 5
+            utilizando un archivo CSV o XLSX.
+          </Typography>
+          <Link href="/prediction" passHref>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ marginTop: "1rem", color: "white" }}
+            >
+              Probar
+            </Button>
+          </Link>
+        </Card>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "1rem",
+            width: "300px",
+            minHeight: "200px",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h5" color="primary" fontWeight="bold">
+            Predecir un archivo
+          </Typography>
+          <Typography variant="body1" sx={{ wordWrap: "break-word" }}>
+            Predice las opiniones ciudadanas respecto a los ODS 3, 4 y 5 a
+            partir de los textos ingresados por el usuario.
+          </Typography>
+          <Link href="/prediction" passHref>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ marginTop: "1rem", color: "white" }}
+            >
+              Probar
+            </Button>
+          </Link>
+        </Card>
       </Grid2>
     </Container>
   );
